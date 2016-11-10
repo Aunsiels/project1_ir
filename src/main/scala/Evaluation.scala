f75107fba12c73616d627d836fa3be361e06c68c
Julien <romerojulien34@gmail.com>
2016-11-10 18:41:46 +0100

    correction evaluation class

--- src/main/scala/Evaluation.scala
+++ src/main/scala/Evaluation.scala
@@ -10,30 +10,29 @@ object Evaluation {
             "Recall    : " + recall    + "\n" +
             "F1        : " + f1
         }
+
+        def add (that : Stat) = {
+            Stat(that.precision + this.precision,
+                that.recall + this.recall,
+                that.f1 + this.f1)
+        }
     }
 
 
     // retriev = prediction
     def getStat[A] (retriev : Set[A], relev : Set[A], beta : Double) : Stat = {
         val truePos = (relev & retriev).size.toDouble
-        val precision = truePos / retriev.size
-        val recall = truePos / relev.size
-        val f1 = (beta * beta + 1) * precision * recall / (beta * beta * precision + recall)
+        val precision = if (retriev.size > 0) truePos / retriev.size else 0.0
+        val recall = if (relev.size > 0) truePos / relev.size else 0.0
+        var f1 = 0.0
+        if (truePos != 0)
+            f1 = (beta * beta + 1) * precision * recall / (beta * beta * precision + recall)
         Stat(precision, recall, f1)
     }
 
     def getStat[A] (retrievs : Iterable[Set[A]], relevs : Iterable[Set[A]], beta : Double) : Stat = {
-        var precisionCount = 0.0
-        var recallCount = 0.0
-        var nPositive = 0.0
-        for ((retriev, relev) <- retrievs.zip(relevs)){
-            precisionCount += retriev.size
-            recallCount += relev.size
-            nPositive += (relev & retriev).size.toDouble
-        }
-        val precision = nPositive / precisionCount
-        val recall = nPositive / recallCount
-        val f1 = (beta * beta + 1) * precision * recall / (beta * beta * precision + recall)
-        Stat(precision, recall, f1)
+        val sumStats = retrievs.zip(relevs).foldLeft(Stat(0.0, 0.0, 0.0))((z, s) => z.add(getStat(s._1, s._2, beta)))
+        val size = relevs.size.toDouble
+        Stat(sumStats.precision / size, sumStats.recall / size, sumStats.f1 / size)
     }
 }
