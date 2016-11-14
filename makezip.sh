16fe3f39bb799d98fd9d5f638da101e952dde754
mmgreiner <mmgreiner@bluewin.ch>
2016-11-14 23:43:55 +0100

    added readme to zip

--- makezip.sh
+++ makezip.sh
@@ -2,7 +2,7 @@
 # Information Retrieval, Project 1. Group 11 (Merki, Romero, Greiner).
 # Everything has to be zipped to a directory called 'code'
 # Create the zip package by first zipping the relevant folders, then unzipping them to 'code', then zipping again.
-zip -rX temp.zip build.sbt lib src
+zip -rX temp.zip build.sbt lib src README.md
 unzip temp.zip -d code
 zip -rX project1_group11.zip code
 rm -rf code
